3d31dff7891b964aae07cb037f5bacf2d8239315
Masha Burdo <masha.burdo@gmail.com>
2022-03-03 22:30:35 +0300

    second lesson

--- django_tutorial/app_tutorial/models.py
+++ django_tutorial/app_tutorial/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pages_number = models.IntegerField(default=100)
+    description = models.TextField(null=True, blank=True)
+
+
--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'django_tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
