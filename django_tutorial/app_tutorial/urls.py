4af83ca0da5756dc2e16850ff91644864a9fe69e
Masha Burdo <masha.burdo@gmail.com>
2022-03-10 22:32:05 +0300

    third lesson

--- django_tutorial/app_tutorial/urls.py
+++ django_tutorial/app_tutorial/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- django_tutorial/app_tutorial/views.py
+++ django_tutorial/app_tutorial/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Book
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
