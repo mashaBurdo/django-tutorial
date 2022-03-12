3fd9a232a435d3ffc75e9ee29cb256cdc56e21c1
Masha Burdo <masha.burdo@gmail.com>
2022-03-12 15:11:28 +0300

    review detail

--- django_tutorial/app_tutorial/urls.py
+++ django_tutorial/app_tutorial/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('review_detail/<int:review_id>/', views.review_detail, name='review_detail'),
 ]
--- django_tutorial/app_tutorial/views.py
+++ django_tutorial/app_tutorial/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Book
+from .models import Book, Review
 
 
 def index(request):
@@ -11,3 +11,8 @@ def index(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'book_detail.html', {'book': book})
+
+
+def review_detail(request, review_id):
+    review = Review.objects.get(id=review_id)
+    return render(request, 'review_detail.html', {'review': review})
